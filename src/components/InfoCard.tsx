ba1655e68e180e0dad3c1746b23c556a8996ccc5
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-03-14 22:46:48 +0530

    graph

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import graph from "./assets/graphs/graph.svg";
+
 function InfoCard() {
   const [info, setInfo] = useState([] as any);
   const [isLoading, setIsLoading] = useState(true);
@@ -73,73 +75,81 @@ function InfoCard() {
         <div className="bg-darkGray lg:w-[50vw]"></div>
 
         {/* third section */}
-        <div className="lg:w-[30vw] flex lg:flex-row flex-col gap-2">
-          {/* total deaths */}
-          <div className="bg-darkGray lg:w-[20vw] flex flex-col justify-start items-center font-sans pt-[15px]">
-            <p className="font-light text-xl">Total Deaths</p>
-            <p className=" lg:text-6xl text-3xl">18291</p>
+        <div className="lg:w-[30vw] flex flex-col gap-2 justify-between">
+          <div className=" flex lg:flex-row flex-col gap-2">
+            {/* total deaths */}
+            <div className="bg-darkGray lg:w-[20vw] flex flex-col justify-start items-center font-sans pt-[15px]">
+              <p className="font-light text-xl">Total Deaths</p>
+              <p className=" lg:text-6xl text-3xl">18291</p>
 
-            <div className="h-[292px] w-full overflow-y-auto mt-[15px]">
-              {isLoading ? (
-                <p className="text-base text-center">Loading...</p>
-              ) : (
-                <>
-                  {info.map((item: any) => (
-                    <div className="flex flex-col border-b border-[#464646] p-1 px-2">
-                      <p className="text-sm">
-                        {item?.deaths?.total}{" "}
-                        <span className="font-light">deaths</span>
-                      </p>
-                      <p className="text-sm">
-                        <span className="font-semibold font-sans">
-                          {item?.country}
-                        </span>
-                        ,{" "}
-                        <span className="text-lightGray">
-                          {item?.continent}
-                        </span>
-                      </p>
-                    </div>
-                  ))}
-                </>
-              )}
+              <div className="h-[310px] w-full overflow-y-auto mt-[15px]">
+                {isLoading ? (
+                  <p className="text-base text-center">Loading...</p>
+                ) : (
+                  <>
+                    {info.map((item: any) => (
+                      <div className="flex flex-col border-b border-[#464646] p-1 px-2">
+                        <p className="text-sm">
+                          {item?.deaths?.total}{" "}
+                          <span className="font-light">deaths</span>
+                        </p>
+                        <p className="text-sm">
+                          <span className="font-semibold font-sans">
+                            {item?.country}
+                          </span>
+                          ,{" "}
+                          <span className="text-lightGray">
+                            {item?.continent}
+                          </span>
+                        </p>
+                      </div>
+                    ))}
+                  </>
+                )}
+              </div>
+            </div>
+
+            {/* total recovered */}
+            <div className="bg-darkGray lg:w-[20vw] flex flex-col justify-start items-center font-sans pt-[15px]">
+              <p className="font-light text-xl ">Total Recovered</p>
+              <p className="text-matteGreen lg:text-6xl text-3xl">19228</p>
+
+              <div className="h-[310px] w-full overflow-y-auto mt-[15px]">
+                {isLoading ? (
+                  <p className="text-base text-center">Loading...</p>
+                ) : (
+                  <>
+                    {info.map(
+                      (item: any) =>
+                        item?.cases?.recovered && (
+                          <div className="flex flex-col border-b border-[#464646] p-1 px-2">
+                            <p className="text-sm text-matteGreen">
+                              {item?.cases?.recovered}{" "}
+                              <span className="font-light">recovered</span>
+                            </p>
+                            <p className="text-sm">
+                              <span className="font-semibold font-sans">
+                                {item?.country}
+                              </span>
+                              ,{" "}
+                              <span className="text-lightGray">
+                                {item?.continent}
+                              </span>
+                            </p>
+                          </div>
+                        )
+                    )}
+                  </>
+                )}
+              </div>
             </div>
           </div>
 
-          {/* total recovered */}
-          <div className="bg-darkGray lg:w-[20vw] flex flex-col justify-start items-center font-sans pt-[15px]">
-            <p className="font-light text-xl ">Total Recovered</p>
-            <p className="text-matteGreen lg:text-6xl text-3xl">19228</p>
-
-            <div className="h-[292px] w-full overflow-y-auto mt-[15px]">
-              {isLoading ? (
-                <p className="text-base text-center">Loading...</p>
-              ) : (
-                <>
-                  {info.map((item: any) => (
-                    <div className="flex flex-col border-b border-[#464646] p-1 px-2">
-                      <p className="text-sm text-matteGreen">
-                        {item?.cases?.recovered}{" "}
-                        <span className="font-light">recovered</span>
-                      </p>
-                      <p className="text-sm">
-                        <span className="font-semibold font-sans">
-                          {item?.country}
-                        </span>
-                        ,{" "}
-                        <span className="text-lightGray">
-                          {item?.continent}
-                        </span>
-                      </p>
-                    </div>
-                  ))}
-                </>
-              )}
-            </div>
-          </div>
+          {/* graph */}
+          <img src={graph} alt={graph} />
         </div>
-        
 
+        {/* <div className="w-[30vw] bg-dar"></div> */}
       </div>
     </div>
   );
