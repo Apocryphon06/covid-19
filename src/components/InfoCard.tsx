de0f0ffb3cf62b9fa7ed4eaafb1a684651f60fef
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-03-15 22:41:03 +0530

    layout resize

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -5,7 +5,6 @@ import graph from "./assets/graphs/graph.svg";
 import map from "./assets/graphs/map.svg";
 
 import { Skeleton } from "@mui/material";
-import { padding } from "@mui/system";
 
 function InfoCard() {
   const [info, setInfo] = useState([] as any);
@@ -35,6 +34,7 @@ function InfoCard() {
 
   useEffect(() => {
     getDetails();
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -50,7 +50,7 @@ function InfoCard() {
         <div className="lg:w-[20vw]">
           <div className="bg-darkGray h-[160px] flex flex-col justify-center items-center font-sans">
             <p className="text-2xl font-light ">Total Confirmed</p>
-            <p className="text-glowingRed lg:text-7xl text-5xl">76756</p>
+            <p className="text-glowingRed lg:text-5xl text-4xl">681827072</p>
           </div>
 
           <div className="bg-darkGray font-sans mt-2">
@@ -58,7 +58,7 @@ function InfoCard() {
               Confirmed Cases by Country/Region
             </p>
 
-            <div className="h-[517px] overflow-y-auto">
+            <div className="h-[370px] overflow-y-auto">
               {isLoading ? (
                 <div className="p-4">
                   {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((item: any) => (
@@ -71,7 +71,7 @@ function InfoCard() {
               ) : (
                 <>
                   {info.map((item: any) => (
-                    <div className="flex flex-row justify-between items-center border-b border-[#464646] p-2">
+                    <div className="flex flex-row justify-between items-center border-b border-[#464646] p-[8.2px]">
                       <p className="text-glowingRed text-sm">
                         {item?.cases?.total}
                       </p>
@@ -82,10 +82,27 @@ function InfoCard() {
               )}
             </div>
           </div>
+
+          <div className="bg-darkGray mt-2 font-sans flex flex-col justify-center items-center p-2">
+            <p className="text-lg font-light">Last Updated at (M/D/YYYY)</p>
+            <p className="text-2xl ">2/21/2020, 5:33:03 PM</p>
+          </div>
         </div>
         {/* middle section */}
-        <div className="bg-darkGray lg:w-[50vw]">
-          <img src={map} alt={map}/>
+        <div className="lg:w-[50vw]">
+          <img src={map} alt={map} />
+
+          <div className="bg-darkGray overflow-y-scroll mt-2 flex flex-col justify-start text-justify items-center text-lightGray">
+            <p className="text-sm p-2">
+              Lancet Article: Here. Mobile Version: Here. Visualization: JHU
+              CSSE. Automation Support: Esri Living Atlas team. Data sources:
+              WHO, CDC, ECDC, NHC and DXY. Read more in this blog. Contact US.
+              Downloadable database: GitHub: Here. Feature layer: Here. Point
+              level: City level - US, Canada and Australia; Province level -
+              China; Country level - other countries. Time Zones: lower-left
+              corner indicator - your local time; lower-right corner plot - UTC.
+            </p>
+          </div>
         </div>
 
         {/* third section */}
@@ -94,9 +111,9 @@ function InfoCard() {
             {/* total deaths */}
             <div className="bg-darkGray lg:w-[20vw] flex flex-col justify-start items-center font-sans pt-[15px]">
               <p className="font-light text-xl">Total Deaths</p>
-              <p className=" lg:text-6xl text-3xl">18291</p>
+              <p className=" lg:text-3xl text-3xl">6813670</p>
 
-              <div className="h-[340px] w-full overflow-y-auto mt-[15px]">
+              <div className="h-[300px] w-full overflow-y-auto mt-[15px] py-[17px]">
                 {isLoading ? (
                   // <p className="text-base text-center">Loading...</p>
                   <div className="p-4">
@@ -109,23 +126,26 @@ function InfoCard() {
                   </div>
                 ) : (
                   <>
-                    {info.map((item: any) => (
-                      <div className="flex flex-col border-b border-[#464646] p-1 px-2">
-                        <p className="text-sm">
-                          {item?.deaths?.total}{" "}
-                          <span className="font-light">deaths</span>
-                        </p>
-                        <p className="text-sm">
-                          <span className="font-semibold font-sans">
-                            {item?.country}
-                          </span>
-                          ,{" "}
-                          <span className="text-lightGray">
-                            {item?.continent}
-                          </span>
-                        </p>
-                      </div>
-                    ))}
+                    {info.map(
+                      (item: any) =>
+                        item?.deaths?.total && (
+                          <div className="flex flex-col border-b border-[#464646] p-1 px-2">
+                            <p className="text-sm">
+                              {item?.deaths?.total}{" "}
+                              <span className="font-light">deaths</span>
+                            </p>
+                            <p className="text-sm">
+                              <span className="font-semibold font-sans">
+                                {item?.country}
+                              </span>
+                              ,{" "}
+                              <span className="text-lightGray">
+                                {item?.continent}
+                              </span>
+                            </p>
+                          </div>
+                        )
+                    )}
                   </>
                 )}
               </div>
@@ -134,9 +154,9 @@ function InfoCard() {
             {/* total recovered */}
             <div className="bg-darkGray lg:w-[20vw] flex flex-col justify-start items-center font-sans pt-[15px]">
               <p className="font-light text-xl ">Total Recovered</p>
-              <p className="text-matteGreen lg:text-6xl text-3xl">19228</p>
+              <p className="text-matteGreen lg:text-3xl text-3xl">654908854</p>
 
-              <div className="h-[340px] w-full overflow-y-auto mt-[15px]">
+              <div className="h-[300px] w-full overflow-y-auto mt-[15px] py-[17px]">
                 {isLoading ? (
                   // <p className="text-base text-center">Loading...</p>
                   <div className="p-4">
